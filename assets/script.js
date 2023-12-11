d0383b32104eae57e39aed0b10cf28346e0d2dae
Stephanie Salini <pika2a@hotmail.fr>
2023-12-11 17:37:32 +0100

    modification de la 3eme etape integré avec javaScript et non par html

--- assets/script.js
+++ assets/script.js
@@ -21,10 +21,35 @@ const slides = [
 // recuperer la fleche de gauche
 let flecheDeGauche = document.querySelector(".arrow_left");
 flecheDeGauche.addEventListener("click", function () {
+  for (slides.length = 0; slides.length < 3; slides.length++);
   console.log("vous avez cliqué sur la fleche de gauche!");
 });
 
 let flechDeDroite = document.querySelector(".arrow_right");
 flechDeDroite.addEventListener("click", function () {
+  for (slides.length = 0; slides.length < 3; slides.length++);
   console.log("Vous avez cliqué sur la fleche de droite");
 });
+
+let bannerImg = document.querySelector(".banner-img");
+let bannerImgName = bannerImg.src.substring(
+  bannerImg.src.lastIndexOf("/") + 1,
+  bannerImg.src.length
+);
+
+// je cree ma boucle pour integré les bullets
+for (let i = 0; i < slides.length; i++) {
+  //je cree une nouvelle balise
+  let baliseDot = document.createElement("div");
+  // je lui cree une class
+  baliseDot.classList.add("dot");
+
+  //j'integre ma nouvelle balise a son parent
+  let baliseDots = document.querySelector(".dots");
+  baliseDots.appendChild(baliseDot);
+  // si dot sur image alors dot = dot_selected
+
+  if (bannerImgName === slides[i].image) {
+    baliseDot.classList.add("dot_selected");
+  }
+}

8c8cf700c314bd6376e61332e457de3c67193924
Stephanie Salini <pika2a@hotmail.fr>
2023-12-15 09:53:58 +0100

    integration de la dernière étape, les bullets points se mettent à jour pour indiquer a l'utilisateur ou il se trouve sur dans carrousel. Projet terminé

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ const slides = [
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    image: "./assets/images/slideshow//slide2.jpg",
+    image: "./assets/images/slideshow/slide2.jpg",
     tagLine:
       "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
@@ -18,7 +18,7 @@ const slides = [
   },
 ];
 
-// je recupere l'index du tableau slides et je l'nitialise
+// J'nitialise le tableau slides pour recuperer ses index
 let slidesIndex = 0;
 
 //je recupère l'element du DOM banner img
@@ -31,7 +31,7 @@ let slideTagLine = document.querySelector("#banner p");
 let flecheDeDroite = document.querySelector(".arrow_right");
 
 //je crée une fonction pour mettre a jour l'image et le text de mon carrousel
-function slider() {
+function carrousel() {
   slideImg.src = slides[slidesIndex].image;
   slideTagLine.innerHTML = slides[slidesIndex].tagLine;
 }
@@ -45,10 +45,14 @@ flecheDeDroite.addEventListener("click", function () {
   slidesIndex++;
 
   //je fais une conditions a la place de ma boucle, si mon tableau arrive a la fin il revient au debut
+  // Si on dépasse la fin du tableau
   if (slidesIndex >= slides.length) {
+    //je reviens au debut
     slidesIndex = 0;
   }
-  slider();
+  //j'integre mes fonctions pour qu'elles se mettent en place au click
+  carrousel();
+  dotsCarrousel();
   console.log("Vous avez cliqué sur flecheDeDroite");
 });
 
@@ -57,25 +61,37 @@ let flecheDeGauche = document.querySelector(".arrow_left");
 flecheDeGauche.addEventListener("click", function () {
   //je fais la meme condition que la fleche de droite mais je decremente cette fois ci
   slidesIndex--;
+  // Si je dépasse le début du tableau
   if (slidesIndex < 0) {
+    //je vais a la fin
     slidesIndex = slides.length - 1;
   }
-  slider();
+  //j'integre mes fonctions pour qu'elles se mettent en place au click
+  carrousel();
+  dotsCarrousel();
   console.log("Vous avez cliqué sur flecheDeGauche");
 });
 
-//initialiser le carrousel
-slider();
-
-//je ccrée une bouclepour afficher mes bulles
+//je ccrée une boucle pour afficher mes bulles
 for (i = 0; i < slides.length; i++) {
   //je crée un ediv pour contenir mes bulles
   let baliseDot = document.createElement("div");
   baliseDot.classList.add("dot");
   let baliseDots = document.querySelector(".dots");
   baliseDots.appendChild(baliseDot);
-  //je crée la class dot_selected pour se positionner dans le carrousel
+  /*//je crée la class dot_selected pour se positionner dans le carrousel
   let dotActive = document.querySelector(".dot");
-  dotActive.classList.add("dot_selected");
-  // si je passe a la prochaine image dot_selected change de position
+  dotActive.classList.add("dot_selected");*/
+}
+function dotsCarrousel() {
+  // je recupere tout les dot
+  let dots = document.querySelectorAll(".dot");
+
+  //boucle pour que modifier la class dot => dot selected
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("dot_selected");
+  }
+
+  //je crée la class dot-selected
+  dots[slidesIndex].classList.add("dot_selected");
 }
